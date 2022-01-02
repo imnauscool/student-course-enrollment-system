988f3f1e44f903511b1397da45f0f43407d2f880
navneet <imnauscool@gmail.com>
2022-01-02 22:51:19 +0530

    student course enrollment system

---
+++ studentdatabaseapp/overriding.java
@@ -0,0 +1,16 @@
+package studentdatabaseapp;
+
+// Overriding method
+public class overriding {
+    public void myMethod() {
+        System.out.println("***************************************************");
+    }
+}
+
+//Inheritance
+class Demo extends overriding {
+    public void myMethod() {
+        //Super keyword
+        super.myMethod();
+    }
+}
